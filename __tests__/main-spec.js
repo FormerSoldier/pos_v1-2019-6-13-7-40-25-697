202adc4c05b9fa9e4bd3a82d5dd981857bac372a
TYG <793557590@qq.com>
2019-07-10 22:50:01 +0800

    test: test for function statisticsCountByBarcodes

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -9,3 +9,8 @@ it('should return [] when call isBarcodeValid given ["ITEM000005","ITEM000001"]'
 it('should return ["ITEM000006"] when call isBarcodeValid given ["ITEM000005","ITEM000006"]',() =>{
     expect(obj.isBarcodeValid(items,['ITEM000005','ITEM000006'])).toEqual(['ITEM000006']);
 });
+
+// test cases for function statisticsCountByBarcodes
+it('should return [{barcode:"ITEM000005",count:1},{barcode: "ITEM000001",count:2}] when call isBarcodeValid given ["ITEM000005","ITEM000001","ITEM000001"]',() =>{
+    expect(obj.statisticsCountByBarcodes(['ITEM000005','ITEM000001','ITEM000001'],['ITEM000005','ITEM000006'])).toEqual([{barcode:'ITEM000005',count:1},{barcode: 'ITEM000001',count:2}]);
+});
