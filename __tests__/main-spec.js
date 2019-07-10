e63df159fd8b0120ebab619aa9fb399404da9ec5
TYG <793557590@qq.com>
2019-07-10 22:57:37 +0800

    test,refeat:add a test case for function transformWithMoreInfo

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -11,6 +11,13 @@ it('should return ["ITEM000006"] when call isBarcodeValid given ["ITEM000005","I
 });
 
 // test cases for function statisticsCountByBarcodes
-it('should return [{barcode:"ITEM000005",count:1},{barcode: "ITEM000001",count:2}] when call isBarcodeValid given ["ITEM000005","ITEM000001","ITEM000001"]',() =>{
+it('should return [{barcode:"ITEM000005",count:1},{barcode: "ITEM000001",count:2}] when call statisticsCountByBarcodes given ["ITEM000005","ITEM000001","ITEM000001"]',() =>{
     expect(obj.statisticsCountByBarcodes(['ITEM000005','ITEM000001','ITEM000001'],['ITEM000005','ITEM000006'])).toEqual([{barcode:'ITEM000005',count:1},{barcode: 'ITEM000001',count:2}]);
+});
+
+// test cases for function transformWithMoreInfo
+it('should return [{barcode:"ITEM000005",count:1},{barcode: "ITEM000001",count:2}] when call transformWithMoreInfo given ["ITEM000005","ITEM000001","ITEM000001"]',() =>{
+    expect(obj.transformWithMoreInfo(items,[{barcode:'ITEM000005',count:1},{barcode: 'ITEM000001',count:2}]))
+        .toEqual([{barcode:'ITEM000005',name:'方便面',unit:'袋',price:4.50,count:1},
+        {barcode:'ITEM000001',name:'雪碧',unit:'瓶',price:3.00,count:2}]);
 });
